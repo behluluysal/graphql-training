118168584d1816565e3609949d01d2c349cab8d0
behlül uysal <s.b.uysal98@hotmail.com>
2020-10-10 17:04:37 +0300

    args, parent usage

--- app.js
+++ app.js
@@ -1,15 +1,96 @@
 const { GraphQLServer } = require('graphql-yoga');
 
+const Users = [
+  {
+    id:1,
+    username:'Behlul',
+    city:'Sakarya'
+  },
+  {
+    id:2,
+    username:'Yasin',
+    city:'İstanbul'
+  },
+  {
+    id:1,
+    username:'Caner',
+    city:'Sivas'
+  }
+];
+
+const Posts = [
+  {
+    id:1,
+    text:'Behlul text',
+    userId:1
+  },
+  {
+    id:2,
+    text:'Yasin text',
+    userId:2
+  },
+  {
+    id:1,
+    text:'Caner text',
+    userId:3
+  }
+];
+
+
 const typeDefs = `
   type Query {
-    hello: String!
+    getUser(id: ID!): User!
+    getUsers:[User!]!
+
+    getpOST(id:ID!): Post!
+    getpOSTs: [Post!]! 
+
+    getUserPosts(id:ID!): Combined!
   }
+
+  type User{
+    id: ID!,
+    username:String!,
+    city:String!
+  }
+  type Post{
+    id: ID!,
+    text:String!,
+    userId:ID!
+  }
+  type Combined{
+    id: ID!,
+    username:String!,
+    city:String!,
+    posts : Post
+  }
+ 
 `;
 
 const resolvers = {
-  Query: {
-    hello: () => 'hellow world',
+  Query:{
+    getUser: (parent, args)=>{
+      return Users.find(getUser=>getUser.id == args.id);
+    },
+    getUsers: (parent, args)=>{
+      return Users;
+    },
+    getpOST: (parent, args)=>{
+      return Posts.find(getpOST=>getpOST.id == args.id);
+    },
+    getpOSTs: (parent, args)=>{
+      return Posts;
+    },
+    getUserPosts: (parent, args)=>{
+      return Users.find(getUser=>getUser.id == args.id);
+    }
   },
+  Combined:{
+    posts: (parent, args)=>{
+      return Posts.find(getpOST=>getpOST.userId == parent.id);
+    },
+  }
+
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
